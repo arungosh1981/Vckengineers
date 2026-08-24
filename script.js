326eebcda7185ba7ab1a4d8b086a2dbef3988070
arungosh1981 <arungosh1234@gmail.com>
2026-08-25 03:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-<script src="script.js"></script>
+function toggleMenu() {
+  const nav = document.getElementById("navbar");
+  nav.classList.toggle("show");
+}
+
+/* Close mobile menu after selecting a section */
+
+document.querySelectorAll("#navbar a").forEach(function(link) {
+  link.addEventListener("click", function() {
+    document.getElementById("navbar").classList.remove("show");
+  });
+});
